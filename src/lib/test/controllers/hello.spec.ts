8f5075b72a91c8cbfdec36abfb08828b7223a400
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-10-04 11:48:07 +0300

    configure project to use mocha && chai

---
+++ src/lib/test/controllers/hello.spec.ts
@@ -0,0 +1,10 @@
+import { hello } from "../../controllers/hello";
+import { expect } from "chai";
+import "mocha";
+
+describe("Hello function", () => {
+  it("should return hello world", () => {
+    const result = hello();
+    expect(result).to.equal("Hello world!");
+  });
+});
