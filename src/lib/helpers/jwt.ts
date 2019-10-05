2e5528c8b0b3872547cdc3aa3900afb128008e89
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-10-05 10:28:04 +0300

    generate && verify jwt tokens

---
+++ src/lib/helpers/jwt.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt from 'jsonwebtoken';
+import { Response, Request, NextFunction } from 'express';
+
+class JWT {
+    private SECRETE_KEY: any = 'hehefdhdefhefehfeyteeftegte';
+    /**
+        Create token for users
+        @params { none }
+        
+    */
+    public tokenAdminUser = () => {
+        const token = jwt.sign({}, this.SECRETE_KEY, { expiresIn: '24h' });
+        return token;
+    };
+
+    /**
+        @params { req, res, next }
+     */
+    public verifyToken = (req: Request, res: Response, next: NextFunction) => {
+        const token: any = req.headers['x-access-token'] || req.headers.authorization;
+        if (!token) {
+            return res.status(401).json({ status: 401, message: 'No token provided.' });
+        }
+        jwt.verify(token, this.SECRETE_KEY, (err: any) => {
+            if (err) {
+                return res.status(401).json({ status: 401, message: 'Failed to authenticate token.' });
+            }
+        });
+        next();
+    };
+}
+
+export default new JWT();
