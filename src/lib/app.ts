46485e973ed3a96ca90d05fef8678bb9bffe4b70
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2019-10-06 21:21:59 +0300

    feat(logging): winston && morgan  - Use winston && morgan for logging
    [Finishes #168972218]

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import JSONvalidator from './helpers/validateJSON';
 import AuthRoutes from './routes';
+import morgan from 'morgan';
+import winston from './helpers/winston';
 
 class App {
     public app: express.Application = express();
@@ -10,6 +12,7 @@ class App {
     public constructor() {
         this.app.use(cors());
         this.config();
+        this.app.use(morgan('combined', { stream: winston.stream }));
         this.app.use(JSONvalidator.tryParseJSON);
         this.configureRoutes();
     }
